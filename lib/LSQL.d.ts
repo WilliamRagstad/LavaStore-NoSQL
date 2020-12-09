2de29397dd0e2826fd84941ab17a0162e22ce42c
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-09 16:59:02 +0100

    Build 1.0.4

--- lib/LSQL.d.ts
+++ lib/LSQL.d.ts
@@ -1,10 +1,27 @@
 import { LSDocument } from "lavastore";
 declare class LSQL {
     private document;
+    private newID;
     constructor(document: LSDocument);
     Select(query: {
         from: string;
         where?: object;
+        limit?: number;
     }): LSDocument[];
+    Insert(query: {
+        to: string;
+        document?: LSDocument;
+        data?: object;
+    }): boolean;
+    Update(query: {
+        in: string;
+        where?: object;
+        set?: object;
+    }): void;
+    Delete(query: {
+        from: string;
+        where?: object;
+        limit?: number;
+    }): number;
 }
 export default LSQL;
